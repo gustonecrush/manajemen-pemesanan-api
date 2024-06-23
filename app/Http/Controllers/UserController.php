aed1f6719574d753528a391beca4cb567b07f978
gustonecrush <codeboxezz@gmail.com>
2024-06-24 01:33:43 +0700

    fixed bugs login token

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -39,7 +39,7 @@ class UserController extends Controller
             return response()->json(['error' => 'Unauthorized'], 401);
         }
 
-        return $this->respondWithToken($token);
+        return $this->respondWithToken(JWTAuth::fromUser($credentials));
     }
 
     public function logout()
@@ -56,7 +56,7 @@ class UserController extends Controller
     protected function respondWithToken($token)
     {
         return response()->json([
-            'access_token' => $token,
+            'token' => $token,
             'token_type' => 'bearer',
             'expires_in' => auth('api')->factory()->getTTL() * 60
         ]);
